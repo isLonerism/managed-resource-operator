221c634bf95be487508f7551bc17eccac5333625
isLonerism <isLonerism>
2020-08-23 21:44:32 +0300

    feat(defined valid managed states)

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,1 +1,12 @@
 package utils
+
+// ManagedState is a custom state type for a managed resource
+type ManagedState string
+
+// Valid states for a managed resource
+const (
+	StateEnabled  = "Enabled"
+	StateDisabled = "Disabled"
+	StateDenied   = "Denied"
+	StateError    = "Error"
+)
